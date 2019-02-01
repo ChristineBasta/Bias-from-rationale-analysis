95a5e38d0391ecb4372f741fdf24dccf8f2d95d1
Hanwen Jiang <32667116+jiangolder@users.noreply.github.com>
2019-02-01 11:40:14 +0800

    Update gender_sentiment.py

--- rationale model/rationale_net/datasets/gender_sentiment.py
+++ rationale model/rationale_net/datasets/gender_sentiment.py
@@ -12,7 +12,7 @@ SMALL_TRAIN_SIZE = 800
 @RegisterDataset('gender_sentiment')
 class LongGenderDataset(AbstractDataset):
 
-    def __init__(self, args, word_to_indx, mode, max_length=800, stem='raw_data/gender_sentiment/Mturk_combined_'):
+    def __init__(self, args, word_to_indx, mode, max_length=800, stem='raw_data/gender_sentiment/all_sent_'):
         #aspect = args.aspect
         self.args= args
         self.name = mode
@@ -22,7 +22,7 @@ class LongGenderDataset(AbstractDataset):
         self.max_length = max_length
         #self.aspects_to_num = {'appearance':0, 'aroma':1, 'palate':2,'taste':3, 'overall':4}# 5 kind of labels
         #self.class_map = {0: 0, 1:0, 2:0, 3:0, 4:1, 5:1, 6:1, 7:1, 8:2, 9:2, 10:2}# mapping to 3 class
-        self.name_to_key = {'train':'ucla', 'dev':'ucla', 'test':'ucla'}
+        self.name_to_key = {'train':'obftrain', 'dev':'obftrain', 'test':'test'}
         self.class_balance = {}
         #with gzip.open(stem+str(self.aspects_to_num[aspect])+'.'+self.name_to_key[self.name]+'.txt.gz') as gfile:
         with gzip.open(stem + self.name_to_key[self.name] + '.txt.gz') as gfile:
@@ -31,11 +31,11 @@ class LongGenderDataset(AbstractDataset):
             if args.debug_mode:
                 lines = lines[:SMALL_TRAIN_SIZE]
             elif self.name == 'dev':
-                lines = lines[6300:]
+                lines = lines[300000:305000]
             elif self.name == 'test':
-                lines = lines[:6900]
+                lines = lines[:50000]
             elif self.name == 'train':
-                lines = lines[:6300]
+                lines = lines[:300000]
 
             for indx, line in tqdm.tqdm(enumerate(lines)):
                 uid, line_content = line
@@ -63,7 +63,7 @@ class LongGenderDataset(AbstractDataset):
 
         text_list = line.split('\t')[-1].split()[:self.max_length]
         text = " ".join(text_list)
-        self.args.num_class = 2
+        self.args.num_class = 5
         x = get_indices_tensor(text_list, self.word_to_indx, self.max_length)
         sample = {'text':text,'x':x, 'y':label, 'i':i}
         return sample
